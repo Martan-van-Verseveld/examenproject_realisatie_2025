5f86fbc60a9403fcc192ac97c36a11512702a1bb
Jelmer420 <118176732+Jelmer420@users.noreply.github.com>
2025-01-29 15:10:33 +0100

    #1 Register style 1

--- pages/login.php
+++ pages/login.php
@@ -1,8 +1,14 @@
 <div class="containerlogin">
-    <h1 class="form-title-login">Login</h1>
+    <h1 class="form-title">Login</h1>
     <form action="?page=formHandler" method="post" enctype="multipart/form-data">
         <input type="hidden" name="action" value="login">
-        
+        <div class="form-back">
+            <h1>
+                <a href="?page=home">
+                    <i class="bi bi-arrow-left"></i>
+                </a>
+            </h1>
+        </div>
         <label class="form-label" for="username">Gebruikersnaam</label>
         <input class="form-input" type="text" name="username" placeholder="Username" max="255" min="255">
         <label class="form-label" for="password">Wachtwoord</label>
@@ -11,7 +17,6 @@
         <a href="?page=register">
             <button class="form-button" type="button">Register</button>
         </a>
-        <input class="form-button" type="submit" name="reset" value="Reset">
-
+        <input class="form-button" type="button" name="reset" value="Reset">
     </form>
 </div>
--- pages/register.php
+++ pages/register.php
@@ -1,20 +1,23 @@
-<form action="?page=formHandler" method="post" enctype="multipart/form-data">
-    <input type="hidden" name="action" value="register">
-    <label for="username">Username:</label>
-    <input type="text" name="username" placeholder="Username">
-    <label for="firstname">Voornaam:</label>
-    <input type="text" name="firstname" placeholder="voornaam">
-    <label for="lastname">Achternaam:</label>
-    <input type="text" name="lastname" placeholder="achternaam">
-    <label for="adres">Adres:</label>
-    <input type="text" name="adres" placeholder="adres">
-    <label for="city">Plaats:</label>
-    <input type="text" name="city" placeholder="plaats">
-    <label for="phone">Telefoon:</label>
-    <input type="phone" name="phone" placeholder="telefoon">
-    <label for="email">Email:</label>
-    <input type="email" name="email" placeholder="email">
-    <label for="password">Password:</label>
-    <input type="password" name="password" placeholder="Password">
-    <input type="submit" name="register" value="Register">
-</form>
+<div class="containerregister">
+    <h1 class="form-title">Registeren</h1>
+    <form action="?page=formHandler" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="action" value="register">
+        <label class="form-label" for="username">Gebruikersnaam:</label>
+        <input class="form-input" type="text" name="username" placeholder="Username">
+        <label class="form-label" for="firstname">Voornaam:</label>
+        <input class="form-input" type="text" name="firstname" placeholder="voornaam">
+        <label class="form-label" for="lastname">Achternaam:</label>
+        <input class="form-input" type="text" name="lastname" placeholder="achternaam">
+        <label class="form-label" for="adres">Adres:</label>
+        <input class="form-input" type="text" name="adres" placeholder="adres">
+        <label class="form-label" for="city">Plaats:</label>
+        <input class="form-input" type="text" name="city" placeholder="plaats">
+        <label class="form-label" for="phone">Telefoon:</label>
+        <input class="form-input" type="phone" name="phone" placeholder="telefoon">
+        <label class="form-label" for="email">Email:</label>
+        <input class="form-input" type="email" name="email" placeholder="email">
+        <label class="form-label" for="password">Wachtwoord:</label>
+        <input class="form-input" type="password" name="password" placeholder="Password">
+        <input class="form-button" type="submit" name="register" value="Registeren">
+    </form>
+</div>
