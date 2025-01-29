c65bfd212031fbd1105b58da7950065ec800ff34
Jelmer420 <118176732+Jelmer420@users.noreply.github.com>
2025-01-29 13:54:05 +0100

    #1 Login style

--- pages/login.php
+++ pages/login.php
@@ -1,6 +1,13 @@
-<form action="?page=formHandler" method="post" enctype="multipart/form-data">
-    <input type="hidden" name="action" value="login">
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="password" placeholder="Password">
-    <input type="submit" name="login" value="Login">
-</form>
+<div class="containerlogin">
+    <h1 class="form-title-login">Login</h1>
+    <form action="?page=formHandler" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="action" value="login">
+        <label class="form-label" for="username">Gebruikersnaam</label>
+        <input class="form-input" type="text" name="username" placeholder="Username">
+        <label class="form-label" for="password">Wachtwoord</label>
+        <input class="form-input" type="password" name="password" placeholder="Password">
+        <input class="form-button" type="submit" name="login" value="Login">
+        <input class="form-button" type="submit" name="reset" value="Reset">
+        <input class="form-button" type="submit" name="register" value="Register">
+    </form>
+</div>
