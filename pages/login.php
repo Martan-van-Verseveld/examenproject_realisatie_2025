1cf908f216985d00a3624e0acbd795105a658c78
Jelmer420 <118176732+Jelmer420@users.noreply.github.com>
2025-01-29 14:32:33 +0100

    #1 Login 4

--- pages/login.php
+++ pages/login.php
@@ -3,9 +3,9 @@
     <form action="?page=formHandler" method="post" enctype="multipart/form-data">
         <input type="hidden" name="action" value="login">
         <label class="form-label" for="username">Gebruikersnaam</label>
-        <input class="form-input" type="text" name="username" placeholder="Username" max="255" min="255>
+        <input class="form-input" type="text" name="username" placeholder="Username" max="255" min="255">
         <label class="form-label" for="password">Wachtwoord</label>
-        <input class="form-input" type="password" name="password" placeholder="Password" max="255" min="255>
+        <input class="form-input" type="password" name="password" placeholder="Password" max="255" min="255">
         <input class="form-button" type="submit" name="login" value="Login">
         <a href="?page=register">
             <input class="form-button" type="submit" name="register" value="Register">
