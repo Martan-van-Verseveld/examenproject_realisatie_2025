c8ba7f95cc2d17a41c24cc53deff3983907d25cf
martan <martan-van-verseveld@users.noreply.github.com>
2025-01-30 10:28:11 +0100

    .

--- pages/categorie/view.php
+++ pages/categorie/view.php
@@ -7,12 +7,17 @@ use App\Utility\Functions;
 
 $categorie = new Categorie();
 
+if (!isset($_GET['id']) || $_GET['id'] != '') {
+    Functions::jsRedirect(url: '?page=categorie.overzicht');
+}
+
 ?>
 
 
 <section class="divider_50px"></section>
 <section class="main_content">
     <section>
+        <a href="?page=medewerker.overzicht">Terug naar overzicht...</a>
         <?php $categorie->print_categorie($_GET['id']);?>
     </section>
 </section>
--- pages/medewerker/view.php
+++ pages/medewerker/view.php
@@ -7,12 +7,17 @@ use App\Utility\Functions;
 
 $medewerker = new Medewerker();
 
+if (!isset($_GET['id']) || $_GET['id'] != '') {
+    Functions::jsRedirect(url: '?page=medewerker.overzicht');
+}
+
 ?>
 
 
 <section class="divider_50px"></section>
 <section class="main_content">
     <section>
+        <a href="?page=medewerker.overzicht">Terug naar overzicht...</a>
         <?php $medewerker->print_medewerker($_GET['id']);?>
     </section>
 </section>
